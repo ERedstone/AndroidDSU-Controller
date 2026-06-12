2ab010b268c65a4b3ba907f8b00f8ea063b3c195
ERedstone <103945885+ERedstone@users.noreply.github.com>
2026-06-12 07:43:43 +0000

    Create UsbReceiver.java

---
+++ app/src/main/java/com/plawyue/AndroidDSU/UsbReceiver.java
@@ -0,0 +1,12 @@
+package com.plawyue.AndroidDSU;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class UsbReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Handle USB device attached/detached events
+    }
+}
